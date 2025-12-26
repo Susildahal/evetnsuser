c597779bce13782ad826644e4cbdf37080e94da2
Sushil Dahal <susildahal234@gmail.com>
2025-12-26 12:08:34 +0545

    Enhance video element with playsInline and preload
    Added playsInline and preload attributes to video element.

--- src/component/Hero.js
+++ src/component/Hero.js
@@ -73,6 +73,8 @@ export default function HeroSection() {
                         autoPlay
                         muted
                         loop
+                 playsInline preload="auto"
+                            
                     />
 
 
