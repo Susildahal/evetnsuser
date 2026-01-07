7af2720e81a506678f9b1209e241f3c0ac2c5349
sushil dahal <susildahal234@gmail.com>
2026-01-07 13:41:18 +0545

    fix the jsconfig.json

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const serverurl = process.env.NEXT_PUBLIC_API_URL || "http://192.168.10.79:8000/api";
+const serverurl = process.env.NEXT_PUBLIC_API_URL || "https://eventsbackend-drzr.onrender.com/api/";
 const axiosInstance= axios.create({
     baseURL: serverurl, // Replace with your API base URL
     timeout: 10000, // Optional: set a timeout for requests
